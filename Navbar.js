c9c2fb496be0bb5d6e2bfbf6e1e6a672f66fdd9c
anumthapa <anum.roy123@gmail.com>
2020-03-13 00:04:49 +0200

    2 files modified

--- Navbar.js
+++ Navbar.js
@@ -26,7 +26,8 @@ export default class Navbar extends Component {
         <li><a href="#">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user "></span> Your Account</a></li>
+        
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
         <li role ="presentation"><a href="#"><span class="glyphicon glyphicon-shopping-cart badge"></span> Cart</a></li>
       </ul>
     </div>
--- buttonfilemodel.js
+++ buttonfilemodel.js
@@ -4,7 +4,21 @@ export default class Buttonfilemodel extends Component{
   render(){
     return(
         <div className="container">
-          
+            <div className="modal">
+              // heading
+              <div className="modal-heading">
+                    
+                // body
+
+
+
+              // content
+            
+              
+              </div>
+
+
+              </div>
         
         </div>
 
