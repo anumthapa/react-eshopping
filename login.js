ed059a5b1bd44231e5eef33776702cf831779537
anumthapa <anum.roy123@gmail.com>
2020-03-12 22:42:49 +0200

    2 files modified

--- login.js
+++ login.js
@@ -2,10 +2,16 @@ import React, { Component }from "react";
 
 export default class Login extends Component{
   render(){
+
+      // const style={{
+
+      // }
+      // }
+
     return(
       <div className="container ">
-        <div className="row">
-          <div className="col-md-5 mx-auto">
+        <div className="row" >
+          <div className="col-md-5 mx-auto   ">
             <div id ="first">
               <div className="myform form">
                 <div className="logo mb-3">
@@ -19,7 +25,25 @@ export default class Login extends Component{
                       <input type="email" name="email" class="form-control" id="email" aria-describedby="enailHelp" placeholder="Enter Email"></input><br/>
                      <label for="InputPassword">Password </label>
                       <input type="email" name="email" class="form-control" id="email" aria-describedby="enailHelp" placeholder="Enter Password"></input>
-                    
+                    <div className="form group">
+                      <p className="text-center"> By Signing Up you acccept our <a href="#">Terms of Use </a></p>
+                    </div>
+                    <div className="col-md-12 text-center">
+                    <button type="submit" className="btn btn-block mybtn btn-primary tx-tfm">Login</button></div>
+                    <div className="col-md-12">
+                      <div className="login-or">
+                        <hr className="hr-or"></hr>
+                        <span className="span-or">or</span>
+                              </div>
+                      <div className="col-md-12 mb-3">
+                        <p className="text-center">
+                        <a href="#" className="google btn mybtn"><i class="fa fa-google-plus"></i>SignUp Using Google</a></p>
+
+                      </div>
+                      <div className="col-md-12 text-center mb-3">
+                            <button type="submit" className="btn btnblock mybtn btn-primary tx-tfm">SignIn</button>
+                      </div>
+                    </div>
                     </div>
                   
                   </form>
