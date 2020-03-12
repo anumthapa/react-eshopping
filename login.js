6e9e2d650dc7fe94719d433fdc8fa72ffad9ef3a
anumthapa <anum.roy123@gmail.com>
2020-03-12 16:03:09 +0200

    1 file modified

--- login.js
+++ login.js
@@ -1,11 +1,33 @@
-import React, {Component }from "react";
+import React, { Component }from "react";
 
 export default class Login extends Component{
   render(){
     return(
-      
-
-
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5 mx-auto">
+            <div id ="first">
+              <div className="myform form">
+                <div className="logo mb-3">
+                  <div className="col-md-12 text-center">
+                    <h1>Login</h1>
+                  </div>             
+                </div>
+                  <form action="" method="post" name="login">
+                    <div className="form-group">
+                      <label for="InputEmail">Email address </label>
+                      <input type="email" name="email" class="form-control" id="email" aria-describedby="enailHelp" placeholder="Enter email"></input>
+                    
+                    </div>
+                  
+                  </form>
+   
+   
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   };
 
