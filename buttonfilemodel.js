7833bbcd72e2a95330b36a9e630fcf7f2d5686b2
anumthapa <anum.roy123@gmail.com>
2020-03-12 23:59:56 +0200

    1 file added

---
+++ buttonfilemodel.js
@@ -0,0 +1,17 @@
+import React , { Component } from "react";
+
+export default class Buttonfilemodel extends Component{
+  render(){
+    return(
+        <div className="container">
+          
+        
+        </div>
+
+
+    );
+  };
+
+
+
+}
