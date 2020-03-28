dda13a3338d76c68b32a5da9413cc7b21e303aee
anumthapa <anum.roy123@gmail.com>
2020-03-28 08:25:25 +0200

    2 files modified, 1 added

--- Navbar.js
+++ Navbar.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import login from "./login";
 import buttonfilemodal from "./buttonfilemodal";
+import "./Navbar.css";
 export default class Navbar extends Component {
     render(){
          return(
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">E-Shopping</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+   <div className="site-navbar" role="banner">
+   <div className="site-navbar-top">
+    <div className="col-12 col-md-2 py-2">
+       <div className="site-logo">
+          <a href="#" className="logo-shop"> Shoppers <i class="fa fa-font-awesome"></i>	   </a>
+        </div>
+      </div>
+   </div>   
+  </div>
+ <button id="navbarbtn" class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
- 
 <div class="modal fade" id="exampleModal"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
