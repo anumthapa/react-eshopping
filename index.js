d519a40206b67cb414a6a3b7a97c5f9a9229c33c
anumthapa <anum.roy123@gmail.com>
2020-03-28 08:29:28 +0200

    2 binaries uploaded

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ import {Router, Switch} from "react-router-dom";
 import Hello from './Hello';
 import Navbar from "./Navbar";
 import Shop from "./shop";
-import Shopall from "./Shopall";
 import './style.css';
 import Login from "./login";
 import Buttonfilemodel from "./buttonfilemodel";
 import Items from "./items";
-
-
-
 
 class App extends Component {
   constructor() {
@@ -21,27 +17,20 @@ class App extends Component {
     };
   }
 
-
-
-
   render() {
     return (
       <React.Fragment>
       <Navbar/>
-      <Shop/>
-      <Shopall/>
+      <Shop/> 
     </React.Fragment>
 
-      //  <
+      //  <Switch>
 
       //  <Router path ="#" component ={ProductList}/>
       //  <Router path ="/details" component ={Details}/>
       //  <Router path ="/cart" component ={Cart}/>
        
       //  </Switch>
-
-     
-   
     );
   }
 }
--- shop.js
+++ shop.js
@@ -8,9 +8,9 @@ export default class Shop extends Component {
         <div className="container">
            <div className="row py">
            <div className="col-md-12 mb-0">
-            <a href ="#">Home</a>
-            <span classname="mx-4 mb-0">>></span>
-            <strong className="text-black">Shop</strong>
+            <a href ="#"></a>
+            <span classname="mx-4 mb-0"></span>
+            <strong className="text-black"></strong>
            </div>
            </div>
         </div>
