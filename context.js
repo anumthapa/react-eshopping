b80cf89b538335bf098110e173de2fde92715758
anumthapa <anum.roy123@gmail.com>
2020-04-23 01:37:20 +0300

    2 files modified

--- context.js
+++ context.js
@@ -4,9 +4,57 @@ import {storeProducts, detailProduct} from "./data";
 
 export class ProductProvider extends Component {
 state ={
-        
-
+        products : [],
+        detailProduct :  detailProduct,
+        cart: [],
+        modalOpen : true,
+        cartSubTotal: 0,
+        cartTotal: 0
+};
+componentDidMOint(){
+  this.setProducts();
 }
+/* Logic for "singleItem" */
+setProducts = ()=>{
+  let products = [];
+  storeProducts.forEach(item=>{
+    const singleItem ={...item};
+    products= [...products, singleItem];
+  });
+  this.setState(()=>{
+    return(products);
+  }), this.checkCartItems;
+};
+getItem = id =>{
+  const product = this.state.products.find(item => item.id ===id);
+  return product;
+};
+handleDetails = id =>{
+  const product = this.getItem(id);
+  this.setState(()=>{
+    return(detailProduct);
+  });
+}
+/*Open Modal */
+openModal = id=>{
+  const product = this.getItem(id);
+  this.setState (()=>{
+    return{modalProduct: product, modalOpen: true};
+  });
+};
+  closeModal = ()=>{
+
+  }
 
 
+render(){
+  return(
+    <ProductContext.Provider
+    value={{...this.state,
+    handleDetails:  this.handleDetails,
+    }}
+
+></ProductContext.Provider>
+  );
+}
 }
