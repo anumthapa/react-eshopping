7697de44e2e00abc00c4c797ba0fe534e3199185
anumthapa <anum.roy123@gmail.com>
2020-04-30 21:27:19 +0300

    1 binaries uploaded

--- ShopSingle.js
+++ ShopSingle.js
@@ -1,12 +1,16 @@
 import React , {Component} from "react";
-import Img from "cloth1.jpg"
+import Ig from "https://www.northerntool.com/images/product/2000x2000/508/5087r_2000x2000.jpg";
 export default class ShopSingle extends Component {
   render(){
+    const mystyle= {
+      width: "550px",
+      height:"550px"
+    };
     const {id, title , img , price}= this.props.product;
     return(
    
       <div>
-      <img src={Img}/>
+      <img src={Ig} style={mystyle}/>
       </div>
    
     );
